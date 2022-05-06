4bd1594c01571f23e0b969c6bfb1bbde3f893375
HMimoun <105084525+HMimoun@users.noreply.github.com>
2022-05-06 22:20:32 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashbaord</title>
+</head>
+<body>
+    <div style="width: 20%;height: 615px;color: #ffffff; float: left;  border: 1px solid; background-color: #4e73df; margin-top:0px;">
+        Sidebare </BR>
+        Sidebare
+    </div>
+    <div style="width: 79%;height: 100%;color: #ffffff; float: right;  border: 1px solid; background-color: #4e73df; margin-top:0px;">
+    
+        Rightside
+
+    </div>
+</body>
+</html>
+
